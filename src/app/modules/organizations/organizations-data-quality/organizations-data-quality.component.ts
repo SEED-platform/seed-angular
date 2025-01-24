ae7b3ccc08f20f2d434ebfd3af7cf6f69d421b19
Ross Perry <ross.perry@Rosss-MacBook-Pro-2.local>
2025-01-24 15:25:36 -0700

    inventory specific pages

---
+++ src/app/modules/organizations/organizations-data-quality/organizations-data-quality.component.ts
@@ -0,0 +1,34 @@
+import type { OnInit } from '@angular/core'
+import { Component, inject, ViewEncapsulation } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router'
+import { SharedImports } from '@seed'
+import type { OrganizationTab } from '../organizations.types'
+
+@Component({
+  selector: 'seed-organizations-data-quality',
+  templateUrl: './organizations-data-quality.component.html',
+  encapsulation: ViewEncapsulation.None,
+  imports: [SharedImports],
+})
+export class OrganizationsDataQualityComponent implements OnInit {
+  private _route = inject(ActivatedRoute)
+  private _router = inject(Router)
+
+  readonly tabs: OrganizationTab[] = ['properties', 'taxlots', 'goal']
+  type = this._route.snapshot.paramMap.get('type') as OrganizationTab
+  readonly table_type = 'Data Quality'
+  readonly urlSegment = 'data-quality'
+
+  ngOnInit(): void {
+    console.log('organizations data quality')
+  }
+
+  async toggleInventoryType(type: OrganizationTab) {
+    if (type !== this.type) {
+      const orgId = this._route.snapshot.parent.params.organizationId as string
+      const newRoute = `/organizations/${orgId}/data-quality/${type}`
+      await this._router.navigateByUrl(newRoute, { skipLocationChange: false })
+      this.type = type
+    }
+  }
+}
