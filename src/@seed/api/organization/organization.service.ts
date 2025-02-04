53496a7f6cc4c709b8156337e7e7f08df383de25
Caleb Rutan <caleb.rutan@deptagency.com>
2025-02-04 01:53:36 +0000

    organization backend now active

--- src/@seed/api/organization/organization.service.ts
+++ src/@seed/api/organization/organization.service.ts
@@ -1,22 +1,55 @@
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
 import { inject, Injectable } from '@angular/core'
 import type { Observable } from 'rxjs'
-import { map, ReplaySubject, tap } from 'rxjs'
+import { catchError, map, of, ReplaySubject, Subject, takeUntil, tap } from 'rxjs'
 import { naturalSort } from '../../utils'
-import type { BriefOrganization, Organization, OrganizationsResponse } from './organization.types'
+import { UserService } from '../user'
+import type { BriefOrganization, Organization, OrganizationResponse, OrganizationsResponse } from './organization.types'
+
 
 @Injectable({ providedIn: 'root' })
 export class OrganizationService {
   private _httpClient = inject(HttpClient)
+  private _userService = inject(UserService)
   private _organizations = new ReplaySubject<BriefOrganization[]>(1)
-  organizations$ = this._organizations.asObservable()
+  private _currentOrganization = new ReplaySubject<Organization>(1)
+  private readonly _unsubscribeAll$ = new Subject<void>()
 
-  get(): Observable<Organization[]> {
-    return this._get(false) as Observable<Organization[]>
+  organizations$ = this._organizations.asObservable()
+  currentOrganization$ = this._currentOrganization.asObservable()
+
+  constructor() {
+    // Fetch current org data whenever user org id changes
+    this._userService.currentOrganizationId$.pipe(takeUntil(this._unsubscribeAll$)).subscribe((organizationId) => {
+      this.getById(organizationId).subscribe()
+    })
+  }
+
+  get(org_id?: number): Observable<Organization[]> | Observable<Organization> {
+    if (org_id) {
+      return this.getById(org_id)
+    } else {
+      return this._get(false) as Observable<Organization[]>
+    }
   }
 
   getBrief(): Observable<BriefOrganization[]> {
     return this._get(true)
+  }
+
+  getById(org_id: number): Observable<Organization> {
+    const url = `/api/v3/organizations/${org_id}/`
+    return this._httpClient.get<OrganizationResponse>(url).pipe(
+      map((or) => {
+        this._currentOrganization.next(or.organization)
+        return or.organization
+      }),
+      catchError((error: HttpErrorResponse) => {
+        // TODO need to figure out error handling
+        console.error('Error occurred fetching organization: ', error.error)
+        return of({} as Organization)
+      }),
+    )
   }
 
   private _get(brief = false): Observable<(BriefOrganization | Organization)[]> {
--- src/app/modules/organizations/organizations-settings/organizations-settings.component.ts
+++ src/app/modules/organizations/organizations-settings/organizations-settings.component.ts
@@ -1,17 +1,27 @@
-import type { OnInit } from '@angular/core'
-import { Component, ViewEncapsulation } from '@angular/core'
+import { Component, inject, type OnDestroy, type OnInit } from '@angular/core'
 import { MatIconModule } from '@angular/material/icon'
+import { Subject, takeUntil } from 'rxjs'
+import { type Organization, OrganizationService } from '@seed/api/organization'
+import { SharedImports } from '@seed/directives'
 
 @Component({
   selector: 'seed-organizations-settings',
   templateUrl: './organizations-settings.component.html',
-  encapsulation: ViewEncapsulation.None,
-  imports: [
-    MatIconModule,
-  ],
+  imports: [SharedImports, MatIconModule],
 })
-export class OrganizationsSettingsComponent implements OnInit {
+export class OrganizationsSettingsComponent implements OnInit, OnDestroy {
+  private _organizationService = inject(OrganizationService)
+  organization: Organization
+  private readonly _unsubscribeAll$ = new Subject<void>()
+
   ngOnInit(): void {
-    console.log('organizations settings')
+    this._organizationService.currentOrganization$.pipe(takeUntil(this._unsubscribeAll$)).subscribe((o) => {
+      this.organization = o
+    })
+  }
+
+  ngOnDestroy(): void {
+    this._unsubscribeAll$.next()
+    this._unsubscribeAll$.complete()
   }
 }
