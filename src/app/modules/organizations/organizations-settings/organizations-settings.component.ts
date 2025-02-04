46b8c3746f43fa5a5cd10f485d03853f234ea714
Caleb Rutan <caleb.rutan@deptagency.com>
2025-02-04 02:05:36 +0000

    now with async pipe for simpler componentry

--- src/app/modules/organizations/organizations-settings/organizations-settings.component.ts
+++ src/app/modules/organizations/organizations-settings/organizations-settings.component.ts
@@ -1,27 +1,15 @@
-import { Component, inject, type OnDestroy, type OnInit } from '@angular/core'
+import { CommonModule, NgIf } from '@angular/common'
+import { Component, inject } from '@angular/core'
 import { MatIconModule } from '@angular/material/icon'
-import { Subject, takeUntil } from 'rxjs'
-import { type Organization, OrganizationService } from '@seed/api/organization'
+import { OrganizationService } from '@seed/api/organization'
 import { SharedImports } from '@seed/directives'
 
 @Component({
   selector: 'seed-organizations-settings',
   templateUrl: './organizations-settings.component.html',
-  imports: [SharedImports, MatIconModule],
+  imports: [CommonModule, SharedImports, MatIconModule, NgIf],
 })
-export class OrganizationsSettingsComponent implements OnInit, OnDestroy {
+export class OrganizationsSettingsComponent {
   private _organizationService = inject(OrganizationService)
-  organization: Organization
-  private readonly _unsubscribeAll$ = new Subject<void>()
-
-  ngOnInit(): void {
-    this._organizationService.currentOrganization$.pipe(takeUntil(this._unsubscribeAll$)).subscribe((o) => {
-      this.organization = o
-    })
-  }
-
-  ngOnDestroy(): void {
-    this._unsubscribeAll$.next()
-    this._unsubscribeAll$.complete()
-  }
+  organization$ = this._organizationService.currentOrganization$
 }
