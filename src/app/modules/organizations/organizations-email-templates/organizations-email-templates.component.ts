a54247213e412de6572012a249b509ee02bf2ae8
kflemin <2205659+kflemin@users.noreply.github.com>
2025-02-06 10:25:06 -0700

    start of email templates

--- src/app/modules/organizations/organizations-email-templates/organizations-email-templates.component.ts
+++ src/app/modules/organizations/organizations-email-templates/organizations-email-templates.component.ts
@@ -1,9 +1,13 @@
 import type { OnInit } from '@angular/core'
 import { Component } from '@angular/core'
-
+import { MatButtonModule } from '@angular/material/button'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatIconModule } from '@angular/material/icon'
+import { SharedImports } from '@seed/directives'
 @Component({
   selector: 'seed-organizations-email-templates',
   templateUrl: './organizations-email-templates.component.html',
+  imports: [MatButtonModule, MatDialogModule, MatIconModule, SharedImports],
 })
 export class OrganizationsEmailTemplatesComponent implements OnInit {
   ngOnInit(): void {
