57845ec9a19d021a91c651451528d441f2641934
Caleb Rutan <caleb.rutan@deptagency.com>
2025-02-06 22:35:16 +0000

    first cut at the options form

--- src/app/modules/organizations/settings/options/options.component.ts
+++ src/app/modules/organizations/settings/options/options.component.ts
@@ -1,15 +1,57 @@
 import { CommonModule } from '@angular/common'
-import { Component, inject } from '@angular/core'
+import { Component, inject, type OnDestroy, type OnInit } from '@angular/core'
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'
+import { MatButton } from '@angular/material/button'
+import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatIconModule } from '@angular/material/icon'
-import { OrganizationService } from '@seed/api/organization'
+import { MatInputModule } from '@angular/material/input'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { Subject, takeUntil } from 'rxjs'
+import { type Organization, OrganizationService } from '@seed/api/organization'
+import { type Alert, AlertComponent } from '@seed/components'
 import { SharedImports } from '@seed/directives'
 
 @Component({
   selector: 'seed-organizations-settings-options',
   templateUrl: './options.component.html',
-  imports: [CommonModule, SharedImports, MatIconModule],
+  imports: [CommonModule, SharedImports, MatButton, MatFormFieldModule, MatIconModule, MatInputModule, MatSlideToggleModule, ReactiveFormsModule, AlertComponent],
 })
-export class OptionsComponent {
+export class OptionsComponent implements OnInit, OnDestroy {
   private _organizationService = inject(OrganizationService)
-  organization$ = this._organizationService.currentOrganization$
+  private readonly _unsubscribeAll$ = new Subject<void>()
+  organization: Organization
+  alert: Alert
+  fields: string[] = ['name', 'geocoding_enabled', 'comstock_enabled', 'public_feed_enabled']
+  showAlert = false
+  optionsForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    geocoding_enabled: new FormControl(false, []),
+    comstock_enabled: new FormControl(false, []),
+    public_feed_enabled: new FormControl(false, []),
+  })
+
+  ngOnInit(): void {
+    this._organizationService.currentOrganization$.pipe(takeUntil(this._unsubscribeAll$)).subscribe((organization) => {
+      this.organization = organization
+
+      for (const field of this.fields) {
+        this.optionsForm.get(field).setValue(this.organization[field])
+      }
+      this.optionsForm.get('name').setValue(this.organization.name)
+    })
+  }
+
+  ngOnDestroy(): void {
+    this._unsubscribeAll$.next()
+    this._unsubscribeAll$.complete()
+  }
+
+  submit(): void {
+    console.log('Submitted')
+    this.alert = {
+      type: 'success',
+      message: 'Changes Saved',
+    }
+    this.showAlert = true
+  }
 }
