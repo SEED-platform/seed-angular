d44ed2e7947f2c00807ceb23b6c211ab61d9ab49
Katherine Fleming <2205659+kflemin@users.noreply.github.com>
2025-02-06 10:05:31 -0700

    Profile Pages (#2)
    * start of profile pages
    
    * more profile pages
    
    * fix width
    
    * more profile pages
    
    * fix width
    
    * Lint fixes
    
    * Fixed build
    
    * connect profile info to update user api
    
    * connect developer profile page
    
    * security password page
    
    * validation on info and security forms
    
    * lint
    
    * add password visibility toggle
    
    * display errors when updating user pwd does not work
    
    * simplify password match logic a bit
    
    * remove standalone flag
    
    * lint
    
    * simplify
    
    ---------
    
    Co-authored-by: Alex Swindler <Alex.Swindler@nrel.gov>
    Co-authored-by: Caleb Rutan <caleb.rutan@deptagency.com>

--- src/@seed/api/user/user.service.ts
+++ src/@seed/api/user/user.service.ts
@@ -1,8 +1,16 @@
+import type { HttpErrorResponse } from '@angular/common/http'
 import { HttpClient } from '@angular/common/http'
 import { inject, Injectable } from '@angular/core'
 import type { Observable } from 'rxjs'
-import { distinctUntilChanged, ReplaySubject, switchMap, take, tap } from 'rxjs'
-import type { CurrentUser, SetDefaultOrganizationResponse } from '@seed/api/user'
+import { catchError, distinctUntilChanged, ReplaySubject, switchMap, take, tap, throwError } from 'rxjs'
+import type {
+  CurrentUser,
+  GenerateApiKeyResponse,
+  PasswordUpdateRequest,
+  PasswordUpdateResponse,
+  SetDefaultOrganizationResponse,
+  UserUpdateRequest,
+} from '@seed/api/user'
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
@@ -38,6 +46,55 @@ export class UserService {
       }),
       tap(() => {
         // Refresh user info after changing the organization
+        this.getCurrentUser().subscribe()
+      }),
+    )
+  }
+
+  /**
+   * Update user
+   */
+  updateUser(userId: number, params: UserUpdateRequest): Observable<CurrentUser> {
+    return this._httpClient.put<CurrentUser>(`api/v3/users/${userId}/`, params).pipe(
+      tap((user) => {
+        this._currentUser.next(user)
+      }),
+      catchError((error: HttpErrorResponse) => {
+        console.error('Error occurred while updating user:', error.error)
+        return this._currentUser
+      }),
+    )
+  }
+
+  /**
+   * Update user
+   */
+  updatePassword(params: PasswordUpdateRequest): Observable<PasswordUpdateResponse> {
+    return this.currentUser$.pipe(
+      take(1),
+      switchMap(({ id: userId }) => {
+        return this._httpClient.put<PasswordUpdateResponse>(`api/v3/users/${userId}/set_password/`, params)
+      }),
+      tap(() => {
+        this.getCurrentUser().subscribe()
+      }),
+      catchError((error: HttpErrorResponse) => {
+        return throwError(() => error)
+      }),
+    )
+  }
+
+  /**
+   * Generate API Key
+   */
+  generateApiKey(): Observable<GenerateApiKeyResponse> {
+    return this.currentUser$.pipe(
+      take(1),
+      switchMap(({ id: userId }) => {
+        return this._httpClient.post<GenerateApiKeyResponse>(`api/v3/users/${userId}/generate_api_key/`, {})
+      }),
+      tap(() => {
+        // Refresh user info after changing the API key
         this.getCurrentUser().subscribe()
       }),
     )
