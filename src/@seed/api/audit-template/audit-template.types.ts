c0258e0590825b52ea0f77e1b8177a0ab2c164f8
Caleb Rutan <caleb.rutan@deptagency.com>
2025-02-14 23:59:16 +0000

    put the semicolons back that the linter removed?

--- src/@seed/api/audit-template/audit-template.types.ts
+++ src/@seed/api/audit-template/audit-template.types.ts
@@ -1,22 +1,22 @@
 export type AuditTemplateReportType = {
-  name: string
+  name: string;
 }
 
 export type AuditTemplateConfig = {
-  id: string
-  update_at_day: number
-  update_at_hour: number
-  update_at_minute: number
-  last_update_date?: string
-  organization: number
+  id: string;
+  update_at_day: number;
+  update_at_hour: number;
+  update_at_minute: number;
+  last_update_date?: string;
+  organization: number;
 }
 
 export type AuditTemplateConfigResponse = {
-  status: string
-  data: AuditTemplateConfig[]
+  status: string;
+  data: AuditTemplateConfig[];
 }
 
 export type AuditTemplateConfigCreateResponse = {
-  status: string
-  data: AuditTemplateConfig
+  status: string;
+  data: AuditTemplateConfig;
 }
