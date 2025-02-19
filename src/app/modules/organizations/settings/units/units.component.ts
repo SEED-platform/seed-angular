fee8e6cba2bd48ea6a5b4785bcd9f6817261e06f
Caleb Rutan <crutan@users.noreply.github.com>
2025-02-19 12:30:54 -0500

    Remaining simple org settings (#18)
    * two factor auth form
    
    * ubid threshold settings
    
    * limit to 3 decimals
    
    * title arg
    
    ---------
    
    Co-authored-by: Ross Perry <perryr16@gmail.com>

--- src/app/modules/organizations/settings/units/units.component.ts
+++ src/app/modules/organizations/settings/units/units.component.ts
@@ -1,15 +1,66 @@
 import { CommonModule } from '@angular/common'
-import { Component, inject } from '@angular/core'
+import { Component, inject, type OnDestroy, type OnInit } from '@angular/core'
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { MatButton } from '@angular/material/button'
+import { MatDivider } from '@angular/material/divider'
+import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatIconModule } from '@angular/material/icon'
-import { OrganizationService } from '@seed/api/organization'
+import { MatSelectModule } from '@angular/material/select'
+import { Subject, takeUntil } from 'rxjs'
+import { type Organization, OrganizationService } from '@seed/api/organization'
+import { PageComponent } from '@seed/components'
 import { SharedImports } from '@seed/directives'
+import * as UnitOptions from './units.types'
 
 @Component({
   selector: 'seed-organizations-settings-units',
   templateUrl: './units.component.html',
-  imports: [CommonModule, SharedImports, MatIconModule],
+  imports: [CommonModule, SharedImports, MatButton, MatDivider, MatFormFieldModule, MatIconModule, MatSelectModule, ReactiveFormsModule, PageComponent],
 })
-export class UnitsComponent {
+export class UnitsComponent implements OnDestroy, OnInit {
   private _organizationService = inject(OrganizationService)
-  organization$ = this._organizationService.currentOrganization$
+  private readonly _unsubscribeAll$ = new Subject<void>()
+  betterVerifiedIcon = ''
+  betterVerifiedIconColor = 'primary'
+  organization: Organization
+  options = UnitOptions
+
+  unitsForm = new FormGroup({
+    display_units_eui: new FormControl('', [Validators.required]),
+    display_units_ghg: new FormControl('', [Validators.required]),
+    display_units_ghg_intensity: new FormControl('', [Validators.required]),
+    display_units_water_use: new FormControl('', [Validators.required]),
+    display_units_wui: new FormControl('', [Validators.required]),
+    display_units_area: new FormControl('', [Validators.required]),
+    display_decimal_places: new FormControl(0, [Validators.required]),
+    thermal_conversion_assumption: new FormControl(1, [Validators.min(1), Validators.max(2)]),
+  })
+
+  ngOnInit(): void {
+    this._organizationService.currentOrganization$.pipe(takeUntil(this._unsubscribeAll$)).subscribe((organization) => {
+      this.organization = organization
+      for (const field of Object.keys(this.unitsForm.controls)) {
+        this.unitsForm.get(field).setValue(this.organization[field])
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this._unsubscribeAll$.next()
+    this._unsubscribeAll$.complete()
+  }
+
+  submit(): void {
+    if (this.unitsForm.valid) {
+      this.organization.display_units_eui = this.unitsForm.get('display_units_eui').value
+      this.organization.display_units_ghg = this.unitsForm.get('display_units_ghg').value
+      this.organization.display_units_ghg_intensity = this.unitsForm.get('display_units_ghg_intensity').value
+      this.organization.display_units_water_use = this.unitsForm.get('display_units_water_use').value
+      this.organization.display_units_wui = this.unitsForm.get('display_units_wui').value
+      this.organization.display_units_area = this.unitsForm.get('display_units_area').value
+      this.organization.display_decimal_places = this.unitsForm.get('display_decimal_places').value
+      this.organization.thermal_conversion_assumption = this.unitsForm.get('thermal_conversion_assumption').value
+      this._organizationService.updateSettings(this.organization).subscribe()
+    }
+  }
 }
---
+++ src/app/modules/organizations/settings/units/units.types.ts
@@ -0,0 +1,48 @@
+export const eui = [
+  { label: 'kBtu/sq. ft./year', value: 'kBtu/ft**2/year' },
+  { label: 'GJ/m²/year', value: 'GJ/m**2/year' },
+  { label: 'MJ/m²/year', value: 'MJ/m**2/year' },
+  { label: 'kWh/m²/year', value: 'kWh/m**2/year' },
+  { label: 'kBtu/m²/year', value: 'kBtu/m**2/year' },
+]
+export const ghg = [
+  { label: 'kgCO2e/year', value: 'kgCO2e/year' },
+  { label: 'MtCO2e/year', value: 'MtCO2e/year' },
+]
+
+export const ghg_intensity = [
+  { label: 'kgCO2e/ft²/year', value: 'kgCO2e/ft**2/year' },
+  { label: 'kgCO2e/m²/year', value: 'kgCO2e/m**2/year' },
+  { label: 'MtCO2e/ft²/year', value: 'MtCO2e/ft**2/year' },
+  { label: 'MtCO2e/m²/year', value: 'MtCO2e/m**2/year' },
+]
+
+export const water_use = [
+  { label: 'gal/year', value: 'gal/year' },
+  { label: 'kgal/year', value: 'kgal/year' }, // cspell:disable-line
+  { label: 'L/year', value: 'L/year' },
+]
+
+export const wui = [
+  { label: 'gal/ft²/year', value: 'gal/ft**2/year' },
+  { label: 'kgal/ft²/year', value: 'kgal/ft**2/year' }, // cspell:disable-line
+  { label: 'L/m²/year', value: 'L/m**2/year' },
+]
+
+export const area = [
+  { label: 'square feet', value: 'ft**2' },
+  { label: 'square meters', value: 'm**2' },
+]
+
+export const decimal = [
+  { label: '0 (e.g., 0)', value: 0 },
+  { label: '1 (e.g., 0.1)', value: 1 },
+  { label: '2 (e.g., 0.12)', value: 2 },
+  { label: '3 (e.g., 0.123)', value: 3 },
+  { label: '4 (e.g., 0.1234)', value: 4 },
+]
+
+export const thermal_conversion = [
+  { label: 'US', value: 1 },
+  { label: 'Canada', value: 2 },
+]
