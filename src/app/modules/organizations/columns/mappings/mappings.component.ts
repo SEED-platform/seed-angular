5f01a48f2655b87257f273af9b57619c2d786b87
Caleb Rutan <caleb.rutan@deptagency.com>
2025-04-09 13:40:08 +0000

    try different import method for filesaver

--- src/app/modules/organizations/columns/mappings/mappings.component.ts
+++ src/app/modules/organizations/columns/mappings/mappings.component.ts
@@ -9,7 +9,7 @@ import { MatTooltipModule } from '@angular/material/tooltip'
 import { AgGridAngular } from 'ag-grid-angular'
 import type { CellClassParams, CellDoubleClickedEvent, ColDef, ColGroupDef, GridApi, GridOptions, GridReadyEvent, IRowNode, ValueFormatterParams } from 'ag-grid-community'
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'
-import { saveAs } from 'file-saver'
+import * as FileSaver from 'file-saver'
 import { combineLatest, Subject, takeUntil, tap } from 'rxjs'
 import { type Column, MappableColumnService } from '@seed/api/column'
 import { type ColumnMapping, type ColumnMappingProfile, ColumnMappingProfileService } from '@seed/api/column_mapping_profile/'
@@ -407,7 +407,7 @@ export class MappingsComponent implements OnDestroy, OnInit {
     this._columnMappingProfileService.export(this.mappablePropertyColumns[0].organization_id, this.selectedProfile.id)
       .pipe(takeUntil(this._unsubscribeAll$))
       .subscribe((blob) => {
-        saveAs(blob, filename)
+        FileSaver.saveAs(blob, filename)
       })
   }
 
