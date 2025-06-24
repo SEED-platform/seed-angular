9d1d6f01027350c0f1c9af9c244deaa3009c8ec4
Ross Perry <perryr16@gmail.com>
2025-06-24 22:42:27 +0000

    types

--- src/@seed/api/analysis/analysis.types.ts
+++ src/@seed/api/analysis/analysis.types.ts
@@ -97,3 +97,17 @@ export type ListMessagesResponse = {
   status: 'success';
   messages: AnalysesMessage[];
 }
+
+export type AnalysisSummary = {
+  stats: AnalysisSummaryStats[];
+  number_extra_data_fields: number;
+  status: string;
+  total_records: number;
+}
+
+export type AnalysisSummaryStats = {
+  column_name: string;
+  count: number;
+  display_name: string;
+  is_extra_data: boolean;
+}
