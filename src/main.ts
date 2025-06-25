3fe9cfe33d73d7ca060abaf5f14bd614e0fd4b5d
Ross Perry <perryr16@gmail.com>
2025-06-25 19:08:52 +0000

    temp - register ag gird in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser'
+import { ClientSideRowModelModule, ColumnAutoSizeModule, EventApiModule, ModuleRegistry, PaginationModule, ValidationModule } from 'ag-grid-community'
 import { AppComponent } from 'app/app.component'
 import { appConfig } from 'app/app.config'
+
+// TEMP - should be overwritten when analyses pr is merged
+
+ModuleRegistry.registerModules([
+  ClientSideRowModelModule,
+  ColumnAutoSizeModule,
+  EventApiModule,
+  PaginationModule,
+  ValidationModule,
+])
 
 bootstrapApplication(AppComponent, appConfig).catch((err: unknown) => {
   console.error(err)
