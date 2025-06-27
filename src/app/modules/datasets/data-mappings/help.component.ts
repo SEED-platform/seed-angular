c7a99b4d6772d4b76a4deb72188db34366561e7b
Ross Perry <perryr16@gmail.com>
2025-06-27 17:21:26 +0000

    help component

---
+++ src/app/modules/datasets/data-mappings/help.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'seed-data-mapping-help',
+  templateUrl: './help.component.html',
+  imports: [],
+})
+export class HelpComponent {
+}
