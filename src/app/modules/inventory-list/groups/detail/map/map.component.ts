eaaf6ef7cff1518dbbb040518a7c1c694df00af8
Katherine Fleming <2205659+kflemin@users.noreply.github.com>
2026-05-19 23:36:31 -0600

    map fixes

--- src/app/modules/inventory-list/groups/detail/map/map.component.ts
+++ src/app/modules/inventory-list/groups/detail/map/map.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core'
-import { PageComponent } from '@seed/components'
+import { Component, inject } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { MapComponent } from '../../../map/map.component'
 
 @Component({
   selector: 'seed-group-map',
-  templateUrl: './map.component.html',
-  imports: [PageComponent],
+  template: '<seed-inventory-list-map [groupId]="groupId"></seed-inventory-list-map>',
+  imports: [MapComponent],
+  host: { class: 'flex flex-col flex-auto min-h-0' },
 })
-export class GroupMapComponent {}
+export class GroupMapComponent {
+  groupId = Number.parseInt(inject(ActivatedRoute).parent.snapshot.paramMap.get('groupId'))
+}
